441825449759b6bc27224db8c9448f4d35270928
Takuya Nishida <data9824@users.noreply.github.com>
2017-05-19 11:40:27 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,119 @@
+# optical-kerning.js
+
+*Optical Kerning jQuery Plugin*
+
+This jQuery plugin enables you to apply optical kerning for any text in any fonts, dynamically analyzing the shapes of the text on your browser to apply the "letter-spacing" CSS property on each letter.
+
+あらゆるフォントのあらゆるテキストにオプティカルカーニングを適用できるようにするjQueryプラグインです。ブラウザ上で動的にテキストの形状を分析し、それぞれの文字に "letter-spacing" CSSプロパティを適用します。
+
+![Screenshot](https://raw.githubusercontent.com/data9824/optical-kerning/master/demo/screenshot.png)
+
+## Usage
+
+### Browser
+
+Just place *optical-kerning.js* on your website.
+
+Webサイトに *optical-kerning.js* を配置してください。
+
+```html
+<script src="jquery-{any version}.js" type="text/javascript"></script>
+<script src="optical-kerning.js" type="text/javascript"></script>
+<script type="text/javascript">
+	$(function() {
+		$('.any-selector-you-want').kerning({
+			factor: 0.5,
+			exclude: [[0x00, 0xFF]]
+		});
+	});
+</script>
+```
+
+### Node.js
+
+```
+$ npm install optical-kerning
+```
+
+```javascript
+require('jquery');
+require('optical-kerning');
+$('.any-selector-you-want').kerning({
+	factor: 0.5,
+	exclude: [[0x00, 0xFF]]
+});
+```
+
+## Notice
+
+### Web Fonts
+
+When you use web fonts, you need to (re-)apply kerning after downloading fonts. You can use [Web Font Loader](https://github.com/typekit/webfontloader) to get notified when the fonts have been loaded. See the following example.
+
+Webフォントを利用する際には、フォントをダウンロードした後にカーニングを適用する必要があります。フォントがロードされた時に通知を受けるために、[Web Font Loader](https://github.com/typekit/webfontloader)を利用できます。以下の例をご参照ください。
+
+```javascript
+function update() {
+	$('.kerned').kerning({
+		exclude: [[0x00, 0xFF]]
+	});
+}
+WebFont.load({
+	custom: {
+		families: [ 'Mplus 1p:n4', 'Noto Sans Japanese:n4', 'Sawarabi Mincho:n4' ]
+	},
+	fontactive: function(familyName, fvd) {
+		update();
+	}
+});
+```
+
+### Ligature
+
+Many of Latin fonts have ligatures, custom shapes for specific letter sequences such as "fi". If your font has a ligature, applying this plugin would break the ligature apart making the text ugly. For that reason, it is recommended not to apply this plugin on Latin characters as shown in above usage with *exclude* option.
+
+多くの欧文フォントはリガチャ、すなわち "fi" のような特定の文字列に対する特別な字形を持っています。もしフォントがリガチャを持っていた場合、このプラグインを適用するとリガチャが分断され、テキストが醜くなってしまいます。そのため、上記使用法の *exclude* オプションで示したように、このプラグインは欧文には適用しないことをお勧めします。
+
+### OpenType Features
+
+OpenType fonts can have proportional widths and kerning metrics, which can be enabled with CSS property as follows. However, this plugin doesn't take these property into account. Avoid enabling these features on the element you're applying this plugin on.
+
+OpenTypeフォントは、プロポーショナル幅やカーニングメトリクスを持つことができ、以下のCSSプロパティで有効にできます。しかしながら、このプラグインはこれらのプロパティを考慮しません。このプラグインを適用する要素でこれらの機能を有効にするのは避けてください。
+
+```css
+{
+	font-feature-settings : "pwid" 1, "kern" 1;
+}
+```
+
+## Reference
+
+### kerning()
+
+Applies optical kerning on every descendant of the given jQuery object.
+
+与えられたjQueryオブジェクトの全ての子孫にオプティカルカーニングを適用します。
+
+```javascript
+kerning({options});
+```
+
+*options* is an array that can contain following index and values.
+
+* factor - A factor of kerning strength. 0.0 means no kerning, and 1.0 means kerning is strong enough that adjacent letters are just about to collide. The default value is 0.5.
+* exclude - An array of range or string to specify letters that must not be applied kerning on. The range should contain 2 elements to specify the start and end of the character code that you want to prohibit kerning, inclusive. The string should consist of letters that you want to prohibit kerning. The default value is [].
+
+*options* は以下のインデックスと値を設定できる配列です。
+
+* factor - カーニング強度の係数です。0.0はカーニング無しを意味します。1.0は隣り合う文字がちょうど衝突する強さのカーニングを意味します。デフォルト値は0.5です。
+* exclude - カーニングを適用してはならない文字を指定する、範囲や文字列の配列です。範囲はカーニングを禁止する文字コードの開始と終了の2要素で構成されます（範囲は開始と終了の値も含む）。文字列はカーニングを禁止する文字で構成されます。デフォルト値は[]です。
+
+To remove previously applied kerning, call this function with *options.factor=0*.
+
+以前に適用されたカーニングを除去するには、 *options.factor=0* としてこの関数を呼んでください。
+
+## License
+
+MIT License.
+
+Copyright (c) 2017 Takuya Nishida
---
+++ optical-kerning.js
@@ -0,0 +1,359 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Takuya Nishida
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+(function(factory) {
+	if (typeof define === 'function' && define.amd) {
+        define('optical-kerning', ['jquery'], factory);
+	} else if (typeof module === "object" && typeof module.exports === "object") {
+		module.exports = factory(require("jquery"));
+	} else {
+		factory(jQuery);
+	}
+}(function($) {
+	var excluded_tags = ['option', 'script', 'textarea'];
+	var analyzer = null;
+
+	function Analyzer() {
+		var height = 32;
+		var width = 128;
+		var margin = 16;
+		var tiles = 64;
+		var preparedCache = {};
+		var gapCache = {};
+		var canvas = document.createElement('canvas');
+		canvas.width = width;
+		canvas.height = height * 2 * tiles;
+		canvas.style.display = "none";
+		// calculating gaps is failed for unknown reason for Yu Gothic.
+		// if the canvas is not in the document.
+		document.body.appendChild(canvas);
+		var context = canvas.getContext('2d');
+		context.fillStyle = '#000000';
+		context.textBaseline = 'middle';
+		var image;
+		var imageTop = 0;
+		var analyzeFuncs = [];
+
+		this.prepareGap = function(ch1, ch2, fontStyle, fontWeight, fontFamily, options) {
+			if (constructHash(preparedCache, fontStyle, fontWeight, fontFamily, ch1, ch2, true) === true) {
+				return;
+			}
+			if (imageTop === 0) {
+				context.clearRect(0, 0, width, height * 2 * tiles);
+			}
+			context.font = fontStyle + ' ' + fontWeight + ' ' + height + 'px ' + fontFamily;
+			var ch1Width = context.measureText(ch1).width;
+			context.fillText(ch1, 0, imageTop + height);
+			var center = Math.ceil(ch1Width) + margin;
+			context.fillText(ch2, center + margin, imageTop + height);
+			var top = imageTop;
+			imageTop += height * 2;
+			var analyze = function analyzeImage() {
+				var vertices = convexHull(image, 0, top, center, height * 2, true);
+				var leftBorders = new Array(height * 2);
+				if (vertices.length > 0) {
+					for (var i = 1; i < vertices.length; ++i) {
+						var x0 = vertices[i - 1].x;
+						var y0 = vertices[i - 1].y;
+						var x1 = vertices[i].x;
+						var y1 = vertices[i].y;
+						for (var y = y0; y <= y1; ++y) {
+							var x = x0 + Math.floor((x1 - x0) * (y - y0) / (y1 - y0) + 0.5);
+							leftBorders[y] = x;
+						}
+					}
+				}
+				vertices = convexHull(image, center, top, width - center, height * 2, false);
+				var rightBorders = new Array(height * 2);
+				if (vertices.length > 0) {
+					for (var i = 1; i < vertices.length; ++i) {
+						var x0 = vertices[i - 1].x;
+						var y0 = vertices[i - 1].y;
+						var x1 = vertices[i].x;
+						var y1 = vertices[i].y;
+						for (var y = y0; y >= y1; --y) {
+							var x = x0 + Math.floor((x1 - x0) * (y - y0) / (y1 - y0) + 0.5);
+							rightBorders[y] = x;
+						}
+					}
+				}
+				var gaps = [];
+				for (var y = top; y < top + height * 2; ++y) {
+					if (leftBorders[y] !== undefined && rightBorders[y] !== undefined) {
+						gaps.push(Math.max(0, rightBorders[y] - leftBorders[y] - 1 - margin * 2));
+					}
+				}
+				var gap;
+				if (gaps.length === 0) {
+					var max = -Number.MAX_VALUE;
+					var min = Number.MAX_VALUE;
+					for (var y = top; y < top + height * 2; ++y) {
+						if (leftBorders[y] !== undefined && max < leftBorders[y]) {
+							max = leftBorders[y];
+						}
+						if (rightBorders[y] !== undefined && min > rightBorders[y]) {
+							min = rightBorders[y];
+						}
+					}
+					if (max !== -Number.MAX_VALUE && min !== Number.MAX_VALUE) {
+						gap = (min - max - 1 - margin * 2) / height * options.factor;
+					} else {
+						gap = 0;
+					}
+				} else {
+					var min = gaps[0];
+					for (var i = 0; i < gaps.length; ++i) {
+						min = Math.min(min, gaps[i]);
+					}
+					gap = min / height * options.factor;
+				}
+				constructHash(gapCache, fontStyle, fontWeight, fontFamily, ch1, ch2, gap);
+			};
+			analyzeFuncs.push(analyze);
+			if (analyzeFuncs.length === tiles) {
+				analyzeAll();
+			}
+		};
+
+		this.getGap = function(ch1, ch2, fontStyle, fontWeight, fontFamily) {
+			if (analyzeFuncs.length > 0) {
+				analyzeAll();
+			}
+			return gapCache[fontStyle][fontWeight][fontFamily][ch1][ch2];
+		};
+
+		this.dispose = function() {
+			document.body.removeChild(canvas);
+		}
+
+		function constructHash() {
+			var hash = arguments[0];
+			for (var i = 1; i < arguments.length - 2; ++i) {
+				if (hash[arguments[i]] === undefined) {
+					hash[arguments[i]] = {};
+				}
+				hash = hash[arguments[i]];
+			}
+			var old = hash[arguments[arguments.length - 2]];
+			hash[arguments[arguments.length - 2]] = arguments[arguments.length - 1];
+			return old;
+		}
+
+		function convexHull(image, sx, sy, width, height, right) {
+			var vertices = [];
+			if (right) {
+				for (var y = sy; y < sy + height; ++y) {
+					var i = 3 + 4 * ((sx + width - 1) + y * image.width);
+					for (var x = sx + width - 1; x >= sx; --x, i -= 4) {
+						if (image.data[i] !== 0) {
+							vertices.push({x: x, y: y});
+							break;
+						}
+					}
+				}
+			} else {
+				for (var y = sy + height - 1; y >= sy; --y) {
+					var i = 3 + 4 * (sx + y * image.width);
+					for (var x = sx; x < sx + height; ++x, i += 4) {
+						if (image.data[i] !== 0) {
+							vertices.push({x: x, y: y});
+							break;
+						}
+					}
+				}
+			}
+			var hull = [];
+			for (var i = 0; i < vertices.length; ++i) {
+				hull.push(vertices[i]);
+				while (hull.length >= 3) {
+					var x0 = hull[hull.length - 3].x;
+					var y0 = hull[hull.length - 3].y;
+					var x1 = hull[hull.length - 2].x;
+					var y1 = hull[hull.length - 2].y;
+					var x2 = hull[hull.length - 1].x;
+					var y2 = hull[hull.length - 1].y;
+					if ((x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0) > 0) {
+						break;
+					}
+					hull[hull.length - 2] = hull[hull.length - 1];
+					--hull.length;
+				}
+			}
+			return hull;
+		}
+
+		function analyzeAll() {
+			image = context.getImageData(0, 0, width, height * 2 * tiles);
+			for (var i = 0; i < analyzeFuncs.length; ++i) {
+				analyzeFuncs[i]();
+			}
+			image = null;
+			imageTop = 0;
+			analyzeFuncs.length = 0;
+		}
+		return this;
+	}
+
+	function removeKerning(element) {
+		var text = '';
+		var toRemove = [];
+		function replace() {
+			if (toRemove.length > 0) {
+				node.parentNode.insertBefore(document.createTextNode(text), toRemove[0]);
+				for (var k = 0; k < toRemove.length; ++k) {
+					node.parentNode.removeChild(toRemove[k]);
+				}
+				toRemove.length = 0;
+				text = '';
+			}
+		}
+		for (var node = element.firstChild; node !== null; node = node.nextSibling) {
+			if (node.nodeType === 1) {
+				if (node.className === 'optical-kerning-applied') {
+					text += node.textContent;
+					toRemove.push(node);
+				} else {
+					replace();
+					removeKerning(node);
+				}
+			} else {
+				replace();
+			}
+		}
+		replace();
+		element.normalize();
+	}
+
+	function calcKerning(element, options) {
+		for (var node = element.firstChild; node !== null; node = node.nextSibling) {
+			if (node.nodeType === 1) {
+				if (node.style.letterSpacing !== '') {
+					continue;
+				}
+				if (excluded_tags.indexOf(node.tagName.toLowerCase()) >= 0) {
+					continue;
+				}
+				calcKerning(node, options);
+			} else if (node.nodeType === 3) {
+				if (node.nodeValue.match(/^[\s\t\r\n]*$/)) {
+					continue;
+				}
+				var fontStyle = window.getComputedStyle(node.parentNode).fontStyle;
+				var fontWeight = window.getComputedStyle(node.parentNode).fontWeight;
+				var fontFamily = window.getComputedStyle(node.parentNode).fontFamily;
+				var text = node.nodeValue;
+				for (var i = 0; i < text.length - 1; ++i) {
+					if (excluded(text[i], options.exclude) ||
+						excluded(text[i + 1], options.exclude)
+					) {
+						continue;
+					}
+					analyzer.prepareGap(text[i], text[i + 1],
+						fontStyle, fontWeight, fontFamily, options);
+				}
+			}
+		}
+	}
+
+	function applyKerning(element, options) {
+		for (var node = element.firstChild; node !== null; node = node.nextSibling) {
+			if (node.nodeType === 1) {
+				if (node.style.letterSpacing !== '') {
+					continue;
+				}
+				if (excluded_tags.indexOf(node.tagName.toLowerCase()) >= 0) {
+					continue;
+				}
+				applyKerning(node, options);
+			} else if (node.nodeType === 3) {
+				if (node.nodeValue.match(/^[\s\t\r\n]*$/)) {
+					continue;
+				}
+				var fontStyle = window.getComputedStyle(node.parentNode).fontStyle;
+				var fontWeight = window.getComputedStyle(node.parentNode).fontWeight;
+				var fontFamily = window.getComputedStyle(node.parentNode).fontFamily;
+				var text = node.nodeValue;
+				var spans = document.createDocumentFragment();
+				for (var i = 0; i < text.length - 1; ++i) {
+					if (excluded(text[i], options.exclude) ||
+						excluded(text[i + 1], options.exclude)
+					) {
+						var textNode = document.createTextNode(text[i]);
+						spans.appendChild(textNode);
+						continue;
+					}
+					var gap = analyzer.getGap(text[i], text[i + 1], fontStyle, fontWeight, fontFamily);
+					var span = document.createElement('span');
+					span.setAttribute('class', 'optical-kerning-applied');
+					span.setAttribute('style', 'letter-spacing: ' + (-gap) + 'em');
+					span.textContent = text[i];
+					spans.appendChild(span);
+				}
+				var textNode = document.createTextNode(text[text.length - 1]);
+				spans.appendChild(textNode);
+				spans.normalize();
+				node.parentNode.insertBefore(spans, node);
+				node.parentNode.removeChild(node);
+			}
+		}
+	}
+
+	function excluded(ch, exclude) {
+		var code = ch.charCodeAt(0);
+		for (var i = 0; i < exclude.length; ++i) {
+			if (Array.isArray(exclude[i])) {
+				if (exclude[i][0] <= code && code <= exclude[i][1]) {
+					return true;
+				}
+			} else if (typeof exclude[i] === "string") {
+				for (var k = 0; k < exclude[i].length; ++k) {
+					if (exclude[i].charCodeAt(k) === code) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+	$.fn.kerning = function(options) {
+		options = $.extend({
+			factor: 0.5,
+			exclude: [],
+		}, options);
+		options.factor = Number(options.factor);
+		analyzer = new Analyzer();
+		this.each(function() {
+			removeKerning(this);
+			if (options.factor !== 0.0) {
+				calcKerning(this, options);
+				applyKerning(this, options);
+			}
+		});
+		analyzer.dispose();
+		analyzer = null;
+		return this;
+	};
+	return $.fn.kerning;
+}));
